030fea4062c672c86b36ec6469e6cd8eee5872b9
hyamadev331 <hyamadev331@gmail.com>
2023-06-30 23:59:26 +0900

    Solve Problem

---
+++ P002.sql
@@ -0,0 +1,8 @@
+SELECT 
+ sales_ymd
+,customer_id
+,product_cd
+,amount
+FROM receipt 
+LIMIT 10
+; 
---
+++ P003.sql
@@ -0,0 +1,9 @@
+SELECT 
+ sales_ymd AS sales_date
+,customer_id
+,product_cd
+,amount
+FROM 
+receipt 
+LIMIT 10
+;
